aa72aa637dfcaa5447fdb33446063275713ed895
AIA <mnbvcx0229@gmail.com>
2022-09-28 16:29:11 +0900

    first commit

---
+++ 입문/마지막날짜.java
@@ -0,0 +1,16 @@
+package 입문;
+
+class 마지막날짜{
+    public static void main(String[] args) {
+        마지막날짜 solution = new 마지막날짜();
+        System.out.println(solution.solution());
+    }
+
+    public String solution(){
+        String title = " ### 마지막 날짜 ### ";
+        String answer = "";
+
+        //
+        return title + answer;
+    }
+}
---
+++ 입문/성적표.java
@@ -0,0 +1,14 @@
+package 입문;
+
+class 성적표{
+    public static void main(String[] args) {
+        성적표 solution = new 성적표();
+        System.out.println(solution.solution());
+    }
+    
+    public String solution(){
+        String title = " ### 성적표 ### ";
+        String answer = "";
+        return title + answer;
+    }
+}
